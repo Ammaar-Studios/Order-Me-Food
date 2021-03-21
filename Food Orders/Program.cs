efcfe74478cdc43b325707378e7da375bfebecc3
Ammaar-e <62914394+Ammaar-e@users.noreply.github.com>
2021-03-20 21:10:16 -0600

    Update Program.cs

--- Food Orders/Program.cs
+++ Food Orders/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project1
 {
@@ -14,15 +14,21 @@ namespace Project1
             //Machine Asks For Breakfast
              Console.WriteLine("Hello!, Whats your name?");
             string userName =  Console.ReadLine();   
-            string userBreakfast = Console.ReadLine();
-
             Console.WriteLine("Hello! " + userName + ", Nice to meet you");
+            Console.WriteLine("___________________________________________________________________________________________");
             Console.WriteLine("What Would you like to eat?");
+            string userBreakfast = Console.ReadLine();
             Console.WriteLine("So you would like to eat " +  userBreakfast + ", for breakfast");
             Console.ReadLine();
-            Console.WriteLine("What would you like to drink(say nothing if nothing)");    
+            //_______________________________________________________________________________________________
+            Console.WriteLine("________________________________________________________________________________________________________");
+            Console.WriteLine("What would you like to drink(say nothing if nothing)");
+            string userDrink = Console.ReadLine();
+            Console.WriteLine("So you would like " + userDrink + ", to drink?");
+            Console.ReadLine();    
             //__________________________________________________________________
             //Asks about lunch
+            Console.WriteLine("_____________________________________________________________________________________________________");
             Console.WriteLine("Whats would you like to eat for lunch");
             string userLunch = Console.ReadLine();
 
@@ -31,14 +37,15 @@ namespace Project1
             //_____________________________________________________________________________________________________
             //_____________________________________________________________________________________________________
             // Machine Ask For Dinner
+            Console.WriteLine("______________________________________________________________________________________________________________________________");
             Console.WriteLine("What do you want for Dinner?");
             string userDinner = Console.ReadLine();
 
             Console.WriteLine("Ok so you want to eat " + userDinner + ", for dinner Is this correct?");
             Console.ReadLine();
-
-            string userDrinkSnack = Console.ReadLine();
+            Console.WriteLine("_______________________________________________________________________________________________________________");
             Console.WriteLine("Anything else you would like some more drinks or snacks");
+            string userDrinkSnack = Console.ReadLine();
             Console.WriteLine("So you would like " + userDrinkSnack + ", for A Drink or a Snack");
             Console.ReadLine();
             //_______________________________________________________________________________________________________
@@ -46,9 +53,9 @@ namespace Project1
             //Prints List
             Console.WriteLine("_____________________________________________________________________________________________");
             Console.WriteLine("_____________________________________________________________________________________________");
-            Console.WriteLine("You would like " + userBreakfast + ", for the day");
-            Console.WriteLine("You would like " + userLunch + ", for the day");
-            Console.WriteLine("You would like " + userDinner + ", for the day");
+            Console.WriteLine("You would like " + userBreakfast + ", for BreakFast");
+            Console.WriteLine("You would like " + userLunch + ", for Lunch");
+            Console.WriteLine("You would like " + userDinner + ", for Dinner");
             Console.WriteLine("You would like " + userDrinkSnack + ", for the day");
             Console.WriteLine("Is that what you would like to eat?");
             Console.ReadLine();
